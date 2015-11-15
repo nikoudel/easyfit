5a1874c70d3286e89c28b1c4278f0aa93489d95f
Nikolai Koudelia <nikolai.koudelia@gmail.com>
2015-11-15 15:29:09 +0200

    add Dropwizard Hello World application

--- testWebService/src/main/java/TestWebService.java
+++ testWebService/src/main/java/TestWebService.java
@@ -1,6 +1,37 @@
-public class TestWebService {
- 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TestWebService extends Application<HelloWorldConfiguration>
+{
+    public static void main(String[] args) throws Exception
+    {
+        new TestWebService().run(args);
+    }
+
+    @Override
+    public String getName()
+    {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap)
+    {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment)
+    {
+        final HelloWorldResource resource = new HelloWorldResource(
+            configuration.getTemplate(),
+            configuration.getDefaultName()
+        );
+
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
     }
 }
